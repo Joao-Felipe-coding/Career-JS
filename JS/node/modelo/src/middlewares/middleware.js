0829e8729e70fba34f429c830500af0aef716eba
Joao-Felipe-coding <jofelipe2018@gmail.com>
2025-03-10 16:31:08 -0300

    update files

---
+++ JS/node/modelo/src/middlewares/middleware.js
@@ -0,0 +1,10 @@
+exports.middlewareGlobal = (req, res, next) => {
+  
+  if (req.body.cliente) {
+    console.log();
+    console.log(`Vi que você postou ${req.body.cliente}`);
+    console.log();
+  }
+
+  next();
+}
