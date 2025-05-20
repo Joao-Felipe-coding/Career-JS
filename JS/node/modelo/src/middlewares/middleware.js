ceecbe4a2f02ec09babfa7f2195ab1f159e7e42d
Joao-Felipe-coding <jofelipe2018@gmail.com>
2025-05-20 16:05:11 -0300

    aula sobre injetar conteúdo nos views

--- JS/node/modelo/src/middlewares/middleware.js
+++ JS/node/modelo/src/middlewares/middleware.js
@@ -1,10 +1,4 @@
 exports.middlewareGlobal = (req, res, next) => {
-  
-  if (req.body.cliente) {
-    console.log();
-    console.log(`Vi que você postou ${req.body.cliente}`);
-    console.log();
-  }
-
+  res.locals.umaVariavelLocal = 'Este é o valor da variável local.'
   next();
 }
