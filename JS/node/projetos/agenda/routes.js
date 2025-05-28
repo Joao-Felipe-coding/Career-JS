3189f6bfc43ba0a4bfddc3869a64bcd4013f452e
Joao-Felipe-coding <jofelipe2018@gmail.com>
2025-05-28 14:53:01 -0300

    aplicando captura de dados do form de contatos e salvando no banco de dados

--- JS/node/projetos/agenda/routes.js
+++ JS/node/projetos/agenda/routes.js
@@ -16,6 +16,8 @@ route.post('/login/login', loginController.login);
 route.get('/login/logout', loginController.logout);
 
 //rotas de contato
-route.get('/contato/index', contatoController.index)
+route.get('/contato/index', loginRequired, contatoController.index);
+route.post('/contato/register', loginRequired, contatoController.register);
+route.get('/contato/index/:id', loginRequired, contatoController.editIndex);
 
 module.exports = route;
