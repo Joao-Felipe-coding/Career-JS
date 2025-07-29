87ec002cd562d687fd6426c3b7b5e033dabe3119
Joao-Felipe-coding <jofelipe2018@gmail.com>
2025-07-29 16:02:10 -0300

    criando as primeiras rotas

--- tutorial-react-router-dom/src/main.tsx
+++ tutorial-react-router-dom/src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/global.css'
-import { Home } from './components/Home'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById('root')as HTMLElement).render(
-  <StrictMode>
-    <Home />
-  </StrictMode>,
-)
+//Global style
+import "./styles/global.css";
+
+//Components
+import { Home } from "./components/Home";
+import { About } from "./components/About";
+import { Menu } from "./components/Menu";
+
+createRoot(document.getElementById("root") as HTMLElement).render(
+	<StrictMode>
+		<BrowserRouter>
+			<Menu />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+			</Routes>
+		</BrowserRouter>
+	</StrictMode>,
+);
